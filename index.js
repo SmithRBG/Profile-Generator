90940b635fd1d00246f6ba5c2de0b55250316518
SmithRBG <smit36000@gmail.com>
2021-07-05 20:22:48 -0400

    small updates

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const profileGenerator = require(""); //Connect to others
+const profiles = require(""); //Connect to where the output happens???
 const fs = require("fs");
 const inquirer = require("inquirer");
