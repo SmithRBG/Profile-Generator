0c7e52227db40c3b3062c8bcb470108dc9d4274e
SmithRBG <smit36000@gmail.com>
2021-07-08 18:19:21 -0400

    passed test

--- Test/Enginer.test.js
+++ Test/Enginer.test.js
@@ -3,5 +3,5 @@ const Engineer = require("../Util/engineer")
 test ("Can set github constructor", () => {
     const myTest = "github";
     const eng = new Engineer("Gavin", 1, "Gavin@Jones.com", myTest)
-    expect(eng.Github) .toBe(myTest)
+    expect(eng.github) .toBe(myTest)
 })
