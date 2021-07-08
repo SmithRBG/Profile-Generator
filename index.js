a46c29d456c272ec4a87a70e15b30e20c19b051e
SmithRBG <smit36000@gmail.com>
2021-07-08 10:29:20 -0400

    update app

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const profiles = require(""); //Connect to where the output happens???
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+const employee = require("./Util/employee")
+const engerineer = require("./Util/engineer")
+const manager = require("./Util/manager")
+const intern = require("./Util/intern")
+
+function beginQuestions() {
+    inquirer.prompt([
+        {
+            message: "Please select the type of team member you want to add",
+            name: "Team Member",
+        }
+    ])
+}
